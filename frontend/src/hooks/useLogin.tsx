55eae46bd18fc1de3ca973887c421a60ac2acea1
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-08-05 15:29:29 +0530

    refactor: Add useLogin hook for Signin page and handle form submission

---
+++ frontend/src/hooks/useLogin.tsx
@@ -0,0 +1,63 @@
+import axios from "axios";
+import { useState } from "react";
+import { useAuthContext } from "../context/AuthContext";
+import { Bounce, toast } from "react-toastify";
+
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  const login = async (username: string, password: string) => {
+    const success = handleInputErrors({ username, password });
+    if (!success) {
+      return;
+    }
+    setLoading(true);
+    try {
+      const { data } = await axios.post(
+        "http://localhost:3000/api/auth/signin",
+        {
+          username,
+          password,
+        }
+      );
+      console.log(data);
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      setAuthUser(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { login, loading };
+};
+
+export default useLogin;
+
+type LoginProps = {
+  username: string;
+  password: string;
+};
+
+function handleInputErrors({ username, password }: LoginProps) {
+  if (!username || !password) {
+    toast.error("Fill the input fields", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      transition: Bounce,
+    });
+    return false;
+  }
+  return true;
+}
--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,20 +1,41 @@
+import { useState } from "react"
 import Bottom from "../components/Bottom"
 import Button from "../components/Button"
 import { Heading } from "../components/Heading"
 import InputBox from "../components/InputBox"
 import SubHeading from "../components/SubHeading"
+import useLogin from "../hooks/useLogin"
 
 const Signin = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {loading,login} = useLogin();
+
+  const handleSubmit = async(e:any) =>{
+    e.preventDefault();
+    await login(username,password);
+  }
+
   return (
     <div className="flex h-screen justify-center">
     <div className="flex flex-col justify-center">
       <div className="border border-white border-opacity-15 rounded-lg bg-[#18181B] w-[50vh] text-center p-2 h-max px-4">
           <Heading label="Signin" />
           <SubHeading label="Enter your crendentails" />
-          <InputBox label="Username" placeholder="John Doe"/>
-          <InputBox label="Password" placeholder="123456" type="password"/>
+          <InputBox value={username} onChange={(e)=>{
+            setUsername(e.target.value)
+          }} label="Username" placeholder="John Doe"/>
+          <InputBox value={password} onChange={(e)=>{
+            setPassword(e.target.value)
+          }} label="Password" placeholder="123456" type="password"/>
 
-          <Button label="Sign in" onClick={() => {}}/>
+          <Button
+          disabled={loading}
+          onClick={handleSubmit}
+          className="mt-5"
+           label={loading ? <span className="loading loading-spinner"></span> : "Sign in"
+            }/>
           <Bottom label="Don't have an account?" buttonText="Sign up" to={"/signup"} />
       </div>
     </div>
