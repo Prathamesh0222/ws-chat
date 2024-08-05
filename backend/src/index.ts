1d1c489962e22b38b3d22cedfe631261bbfddf8d
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-08-05 14:51:34 +0530

    chore: Update npm dependencies to include cors v2.8.5

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import connectDB from "./db/db";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/messageRoutes";
 import userRoutes from "./routes/userRoutes";
+import cors from "cors";
 const app = express();
 
 dotenv.config();
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
